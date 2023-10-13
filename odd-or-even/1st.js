3ca23d67728038947dec14a375850ff4994f8910
Mohd Rashid <rashidmohd2274@gmail.com>
2023-10-13 10:04:48 -0700

    new file added

---
+++ odd-or-even/1st.js
@@ -0,0 +1,12 @@
+
+// 1. Program to check number is odd or even
+var x = 24
+
+if(x%2==0){
+    //even
+    console.log(`${x} is an even number`);
+} else{
+    //odd
+    console.log(`${x} is an odd number`)
+}
+
